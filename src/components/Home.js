66a9752888fa9dde0bf058d6896e8c7dce0df24b
Rashmit <rashmitrrd123@gmail.com>
2023-07-14 20:07:21 -0400

    Updated User Guide

--- src/components/Home.js
+++ src/components/Home.js
@@ -264,15 +264,7 @@ const Home = () => {
             User Guide
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+          Welcome to the Sunny the Weather app! Simply enter the name of a city in the search bar, and our website will provide you with real-time weather information for that time and hourly, including temperature, humidity, wind speed, and weather conditions, for that location. Stay informed and plan your day with ease using the Sunny!
           </Typography>
         </Box>
       </Modal>
